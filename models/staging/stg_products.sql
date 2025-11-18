9c512fd5e8b0d2b0fe7f4e8733133b1abed71490
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-18 21:34:05 +0800

    Update stg_products.sql

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -1,6 +1,21 @@
-SELECT
-product_id
-product_name
-product_type
-price
-FROM {{ ref(clean_product_list)  }}
+with counted as (
+    select *,
+           count(*) over (partition by product_id) as cnt
+    from {{ ref('clean_product_list') }}
+    where product_id is not null
+      and price is not null
+),
+
+filtered as (
+    select
+        product_id,
+        product_name,
+        product_type,
+        try_cast(price as numeric) as product_price
+    from counted
+    where cnt = 1
+      and try_cast(price as numeric) is not null
+)
+
+select *
+from filtered
