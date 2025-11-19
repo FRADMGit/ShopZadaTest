448506e94353e060d316b8104d3e604833f6f846
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-19 17:11:17 +0800

    Delete models/products.sql

--- models/products.sql
+++
@@ -1,8 +0,0 @@
-with ranked_products as (
-    select *,
-           rank() over (partition by product_type order by product_price desc) as price_rank
-    from {{ ref('stg_products') }}
-)
-
-select *
-from ranked_products
