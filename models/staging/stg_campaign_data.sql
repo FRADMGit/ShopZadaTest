8b92cc5b9ffe15d9198067bbd11aaf186a48249a
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-19 17:36:01 +0800

    Update stg_campaign_data.sql

--- models/staging/stg_campaign_data.sql
+++ models/staging/stg_campaign_data.sql
@@ -1,15 +1,7 @@
 SELECT
     campaign_id,
     campaign_name,
-    campaign_description,
-    campaign_description_person,
-    campaign_discount
-FROM (
-    SELECT
-        campaign_id,
-        campaign_name,
-        regexp_replace(campaign_description, '\s*-\s*.*$', '') AS campaign_description,
-        regexp_replace(campaign_description, '^.*-\s*', '') AS campaign_description_person,
-        (regexp_replace(discount, '[^0-9\.]', '', 'g')::FLOAT / 100) AS campaign_discount
-    FROM {{ ref('clean_campaign_data') }}
-) AS sub;
+    regexp_replace(campaign_description, '\s*-\s*.*$', '') AS campaign_description,
+    regexp_replace(campaign_description, '^.*-\s*', '') AS campaign_description_person,
+    (regexp_replace(discount, '[^0-9\.]', '', 'g')::FLOAT / 100) AS campaign_discount
+FROM {{ ref('clean_campaign_data') }}
