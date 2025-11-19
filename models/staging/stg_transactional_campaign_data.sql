05c91acc0eef1c881e4e9c6cd3d5707992c3ab6a
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-19 17:48:24 +0800

    Create stg_transactional_campaign_data.sql

---
+++ models/staging/stg_transactional_campaign_data.sql
@@ -0,0 +1,8 @@
+SELECT
+    campaign_id,
+    order_id,
+    transaction_date AS order_transaction_date,
+    REGEXP_REPLACE("estimated arrival", '[^0-9\.]', '', 'g') AS order_arrival_days,
+    availed AS campaign_availed
+FROM
+    {{ ref('clean_transactional_campaign_data') }}
