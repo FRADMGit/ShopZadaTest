70e742ae327ad5fcff50fb33b8d8d74925b85a33
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-19 17:44:51 +0800

    Create stg_line_item_data_prices.sql

---
+++ models/staging/stg_line_item_data_prices.sql
@@ -0,0 +1,9 @@
+SELECT
+    "Unnamed: 0" AS line_id,
+    order_id,
+    price AS line_price,
+    REGEXP_REPLACE(quantity, '[^0-9\.]', '', 'g') AS line_quantity
+FROM
+    {{ ref('clean_line_item_data_prices') }}
+ORDER BY
+    "Unnamed: 0" ASC
