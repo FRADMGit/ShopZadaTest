bd87e42ab76bfbaefeb1d197a7e480228d9a9255
FRADMGit <francisrafaeldelmundo@gmail.com>
2025-11-21 03:23:41 +0800

    Update stg_order_delays.sql

--- models/staging/stg_order_delays.sql
+++ models/staging/stg_order_delays.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT
   order_id,
   "delay in days" AS order_delay
-FROM {{ ref('clean_order_delay') }}
+FROM {{ ref('clean_order_delays') }}
 WHERE order_delay IS NOT NULL
   AND order_id IS NOT NULL
